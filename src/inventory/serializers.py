a850395fbddafd8104ddc055fb8b3a7420f3280f
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-10-15 22:05:50 -0500

    create serializers for producto and categoria

---
+++ src/inventory/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Producto, Categoria
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+
+class ProductoSerializer(serializers.ModelSerializer):
+    categoria_detalle = CategoriaSerializer(source='categoria', read_only=True)
+    
+    class Meta:
+        model = Producto
+        fields = ['id', 'nombre', 'cantidad', 'precio', 'categoria', 'categoria_detalle']
