d6a954971a204f06d8ed32d116146f15a8f9083a
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-10-15 22:08:12 -0500

    create viewsets for producto and categoria

--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Producto, Categoria
+from .serializers import ProductoSerializer, CategoriaSerializer
 
-# Create your views here.
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'categoria__nombre']
+
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
