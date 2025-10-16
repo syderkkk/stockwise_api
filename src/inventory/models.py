f7c39e42d7f5c5b3976a32debf523b9ecdcc1d06
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-10-15 22:01:50 -0500

    add models producto and categoria

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Categorias"
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    cantidad = models.IntegerField(default=0)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+
+    def __str__(self):
+        return self.nombre
